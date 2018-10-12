a1f612d93a931c884a403b78378f19e629b3524c
riiiiii <43875056+riiiiii@users.noreply.github.com>
2018-10-12 17:25:05 +0900

    Update fibonacci.py

---
+++ fibonacci.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Spyderエディタ
+
+これは一時的なスクリプトファイルです
+"""
+a0=0
+a1=1
+a2=a1+a0
+a3=a2+a1
+a4=a3+a2
+a5=a4+a3
+a6=a5+a4
+a7=a6+a5
+a8=a7+a6
+a9=a8+a7
+a10=a9+a8
+
+def fib(n):
+    a0=0
+    a1=1
+    a2=a1+a0
+    return a2
